15494f76f5897c0566854be552fad8f1abdb7900
alionachykal <106018567+alionachykal@users.noreply.github.com>
2022-11-20 15:11:47 +0300

    fixing bugs
    fixing bugs

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,6 +1,6 @@
 export const fetchImages = async (inputValue, pageNr) => {
   return await fetch(
-    `https://pixabay.com/api/?key=31424092-5d0c597189bd463c8c37d4bc2q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
+    `https://pixabay.com/api/?key=29588079-fbc492831fdad231bf7222b96&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
   )
     .then(async response => {
       if (!response.ok) {
